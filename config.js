17267ea815eae3ecb0aed52832f52689824da47c
linlinliu <linlin.liu@ihandysoft.com>
2016-07-04 22:33:04 +0800

    add firebase config

---
+++ config.js
@@ -0,0 +1,6 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyC1qI8bbpbPHAqE-Rfwy24ZyoXOfGyBMzw",
+    authDomain: "plant-330a1.firebaseapp.com",
+    databaseURL: "https://plant-330a1.firebaseio.com",
+    storageBucket: "plant-330a1.appspot.com",
+};
